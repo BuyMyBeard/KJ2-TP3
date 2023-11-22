c1cfbcc8f4ae3268f9c16b73307ec4c1863e3486
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-11-21 21:29:12 -0500

    enemy jump

---
+++ Assets/AgentLinkMover.cs
@@ -0,0 +1,80 @@
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public enum OffMeshLinkMoveMethod
+{
+    Teleport,
+    NormalSpeed,
+    Parabola,
+    Curve
+}
+
+// Code taken from https://forum.unity.com/threads/how-to-trigger-a-jump-on-an-offmesh-link.313628/
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentLinkMover : MonoBehaviour
+{
+    [SerializeField] OffMeshLinkMoveMethod method = OffMeshLinkMoveMethod.Parabola;
+    [SerializeField] AnimationCurve curve = new AnimationCurve();
+    [SerializeField] float duration = 1.0f;
+    
+    IEnumerator Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.autoTraverseOffMeshLink = false;
+        while (true)
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                if (method == OffMeshLinkMoveMethod.NormalSpeed)
+                    yield return StartCoroutine(NormalSpeed(agent));
+                else if (method == OffMeshLinkMoveMethod.Parabola)
+                    yield return StartCoroutine(Parabola(agent, 2.0f, duration));
+                else if (method == OffMeshLinkMoveMethod.Curve)
+                    yield return StartCoroutine(Curve(agent, duration));
+                agent.CompleteOffMeshLink();
+            }
+            yield return null;
+        }
+    }
+    IEnumerator NormalSpeed(NavMeshAgent agent)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        while (agent.transform.position != endPos)
+        {
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    IEnumerator Parabola(NavMeshAgent agent, float height, float duration)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+    IEnumerator Curve(NavMeshAgent agent, float duration)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            float yOffset = curve.Evaluate(normalizedTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+}
